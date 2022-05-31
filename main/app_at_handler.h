c91a8979df2d9b3ea4ff5888239a4c79d999b0b6
cwxiaos <765707041cwres@gmail.com>
2022-06-01 00:14:34 +0800

    Some small change

--- main/app_at_handler.h
+++ main/app_at_handler.h
@@ -12,8 +12,7 @@ static int handler_at(const struct cat_command* cmd){
 static int handler_at_i(const struct cat_command* cmd){
     esp_chip_info_t info;
     esp_chip_info(&info);
-    printf("IDF Version:%s\r\n", esp_get_idf_version());
-    printf("Chip info:\r\n");
+    printf("IDFVersion:%s\r\n", esp_get_idf_version());
     char* chip_name = "Unknown";
     switch(info.model){
         case CHIP_ESP32: chip_name = "ESP32"; break;
@@ -21,15 +20,15 @@ static int handler_at_i(const struct cat_command* cmd){
         case CHIP_ESP32S3: chip_name = "ESP32S3"; break;
         case CHIP_ESP32C3: chip_name = "ESP32C3"; break;
     }
-    printf("\tmodel:%s\r\n", chip_name);
-    printf("\tcores:%d\r\n", info.cores);
-    printf("\tfeature:%s%s%s%s%d%s\r\n",
+    printf("Model:%s\r\n", chip_name);
+    printf("Cores:%d\r\n", info.cores);
+    printf("Feature:%s%s%s%s%d%s\r\n",
            info.features & CHIP_FEATURE_WIFI_BGN ? "/802.11bgn" : "",
            info.features & CHIP_FEATURE_BLE ? "/BLE" : "",
            info.features & CHIP_FEATURE_BT ? "/BT" : "",
            info.features & CHIP_FEATURE_EMB_FLASH ? "/Embedded-Flash:" : "/External-Flash:",
-           spi_flash_get_chip_size() / (1024 * 1024), " MB");
-    printf("\trevision number:%d\r\n", info.revision);
+           spi_flash_get_chip_size() / (1024 * 1024), "MB");
+    printf("RevisionNumber:%d\r\n", info.revision);
     return CAT_RETURN_STATE_OK;
 }
 
